6cbd81463204c752b3740493397f5f6bc4f87121
Gael Robin <gael.robin@gmail.com>
2018-04-26 20:48:03 +0200

    RxSwift setup

--- RxTapCounter/ViewController.swift
+++ RxTapCounter/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    private let bag = DisposeBag()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
